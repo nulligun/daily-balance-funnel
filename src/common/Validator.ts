e1c22a348786d84f0bfb7db2407e483281fbe9c4
Steve Mulligan <steve@nonlocal.ca>
2018-10-20 19:29:59 -0400

    loading state changes async, progress monitor output with completion time

--- src/common/Validator.ts
+++ src/common/Validator.ts
@@ -8,8 +8,10 @@ const config = require("config");
 export class Validator {
     private connection : any;
     private addresses : any;
+    private shouldVerify : boolean;
 
-    constructor(connection:any) {
+    constructor(connection:any, shouldVerify:boolean) {
+        this.shouldVerify = shouldVerify;
         this.connection = connection;
         this.addresses = {};
     }
@@ -37,23 +39,30 @@ export class Validator {
 
                 if (m.unix() > currentDayTimestamp) {
                     resolve({status: "done", block: block.number});
+                    return;
                 }
 
-                self.process(block).then((rawStateChanges: any) => {
-                    let blockBalanceChanges:any = {};
-                    rawStateChanges.forEach((rawStateChange: any) => {
-                        rawStateChange.changes.forEach((change: any) => {
-                            if (!(change.address in blockBalanceChanges)) {
-                                blockBalanceChanges[change.address] = Config.web3.utils.toBN(0);
-                            }
+                let proms = self.process(block);
+                if (proms.length > 0) {
+                    Promise.all(proms).then((rawStateChanges) => {
+                        //self.process(block).then((rawStateChanges: any) => {
+                        let blockBalanceChanges: any = {};
+                        rawStateChanges.forEach((rawStateChange: any) => {
+                            rawStateChange.changes.forEach((change: any) => {
+                                if (!(change.address in blockBalanceChanges)) {
+                                    blockBalanceChanges[change.address] = Config.web3.utils.toBN(0);
+                                }
 
-                            if (change.delta != 0) {
-                                blockBalanceChanges[change.address] = blockBalanceChanges[change.address].add(change.delta);
-                            }
+                                if (change.delta != 0) {
+                                    blockBalanceChanges[change.address] = blockBalanceChanges[change.address].add(change.delta);
+                                }
+                            });
                         });
+                        resolve({block: block.number, changes: blockBalanceChanges});
                     });
-                    resolve({block: block.number, changes: blockBalanceChanges});
-                });
+                } else {
+                    resolve({block: block.number, changes: {}});
+                }
             });
         });
     }
@@ -61,7 +70,7 @@ export class Validator {
     validate(timestamp : any, balances: any) {
         const self = this;
         let ts = timestamp;
-        new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
             Object.keys(balances).forEach((address) => {
                 if (!(address in this.addresses))
                 {
@@ -109,91 +118,52 @@ export class Validator {
                     resolve();
                 }
             });
-        }).then((result) => {
-
         });
     }
 
     private validateBalances(timestamp:any, balance:any, address_id:any)
     {
         const self = this;
-        self.connection.beginTransaction((error:any) => {
-            if (error) throw error;
+        if (this.shouldVerify) {
             self.connection.query("select delta from balances where balance_date = ? and address_id = ?", [timestamp, address_id], function (error: any, results: any, fields: any) {
                 if (error) throw error;
                 if (results.length === 0) {
                     console.log("Balance not found for " + address_id + " on " + timestamp);
-                    self.connection.query("replace into balances (balance_date, address_id, delta) values (?, ?, ?)", [timestamp, address_id, balance.toString()], function (error: any, results: any, fields: any) {
-                        if (error) throw error;
-                        self.connection.commit((error: any) => {
-                            if (error) throw error;
-                        });
-                    });
                 } else {
                     if (results[0].delta !== balance.toString()) {
                         console.log("Balance not a match for " + address_id + " on " + timestamp);
-                        self.connection.query("replace into balances (balance_date, address_id, delta) values (?, ?, ?)", [timestamp, address_id, balance.toString()], function (error: any, results: any, fields: any) {
-                            if (error) throw error;
-                            self.connection.commit((error: any) => {
-                                if (error) throw error;
-                            });
-                        });
                     } else {
                         //console.log("Balance matches for " + address_id + " on " + timestamp);
-                        self.connection.commit((error: any) => {
-                            if (error) throw error;
-                        });
                     }
                 }
-            });
-        });
-    }
-
-    private async process (block: any) {
-        const transactions = block.transactions;
-        const b = block;
-        let endOfDay = moment.unix(block.timestamp).utc().endOf('day').unix();
-        if (transactions.length === 0) return [];
-        const batchLimit = 300;
-        const chunk = (list:any, size:any) => list.reduce((r:any, v:any) =>
-            (!r.length || r[r.length - 1].length === size ?
-                r.push([v]) : r[r.length - 1].push(v)) && r
-            , []);
-        const chunkTransactions = chunk(transactions, batchLimit);
-
-        try {
-            const stateChanges = await Bluebird.map(chunkTransactions, (chunk: any) => {
-                return new Promise((resolve, reject) => {
-                    let completed = false;
-                    const chunkStateChanges:any[] = [];
-
-                    if (chunk.length > 0) {
-                        const batch = new Config.web3.BatchRequest();
-                        chunk.forEach((tx: any) => {
-                            batch.add(Config.web3.getBalanceStateChanges.request(tx.hash, ["stateDiff"], (err: Error, stateChange: any) => {
-                                if (completed) return;
-                                if (!stateChange || err) {
-                                    completed = true;
-                                    reject(err);
-                                }
-                                const taggedStateChange = {changes: stateChange, timestamp: endOfDay};
-                                chunkStateChanges.push(err ? null : taggedStateChange);
-                                if (chunkStateChanges.length >= chunk.length) {
-                                    completed = true;
-                                    resolve(chunkStateChanges);
-                                }
-                            }));
-                        });
-                        batch.execute();
-                    } else {
-                        resolve(chunkStateChanges);
-                    }
+                self.connection.query("replace into balances (balance_date, address_id, delta) values (?, ?, ?)", [timestamp, address_id, balance.toString()], function (error: any, results: any, fields: any) {
+                    if (error) throw error;
                 });
             });
-            return [].concat(...stateChanges)
-        } catch (error) {
-            console.log('Error getting transaction state changes ', error)
-            Promise.reject(error)
+        } else {
+            self.connection.query("replace into balances (balance_date, address_id, delta) values (?, ?, ?)", [timestamp, address_id, balance.toString()], function (error: any, results: any, fields: any) {
+                if (error) throw error;
+            });
         }
+    }
+
+    private process(block: any) {
+        const transactions = block.transactions;
+        let endOfDay = moment.unix(block.timestamp).utc().endOf('day').unix();
+        if (transactions.length === 0) return [];
+
+        return transactions.map((tx:any) => {
+            let thisT = tx;
+            return new Promise((resolve, reject) => {
+                Config.web3.getBalanceStateChanges(thisT.hash, ["stateDiff"], (err: Error, stateChange: any) => {
+                    if (err) reject (err);
+                    if (!stateChange) {
+                        resolve(null);
+                        return;
+                    }
+                    resolve({changes: stateChange, timestamp: endOfDay});
+                });
+            });
+        });
     }
 }
--- src/verify.ts
+++ src/verify.ts
@@ -5,6 +5,15 @@ import moment = require("moment");
 
 const config = require("config");
 const mysql = require("mysql");
+const ProgressInfo = require('progress-info');
+
+const minimist = require('minimist');
+
+let args = minimist(process.argv.slice(2), {
+    default: {
+        v: false
+    }
+});
 
 
 Config.initWeb3();
@@ -20,6 +29,7 @@ const connection = mysql.createConnection({
 });
 
 let maxConcurrentBlocks = parseInt(config.get("PARSER.MAX_CONCURRENT_BLOCKS")) || 5;
+let checkpoint = 1000;
 
 connection.query("select current_full_validate_block from validate_status", function(error:any, results:any, fields:any) {
     let currentBlockNumber = 1;
@@ -31,84 +41,107 @@ connection.query("select current_full_validate_block from validate_status", func
     Config.web3.eth.getBlock(currentBlockNumber, true).then((firstBlock:any) => {
         const m = moment.unix(firstBlock.timestamp).utc().endOf('day');
         let currentDayTimestamp = m.unix();
-        const starter = new Validator(connection);
+
+        const starter = new Validator(connection, args.v);
         starter.loadAddresses().then(() => {
             console.log("Addresses loaded");
             Config.web3.eth.getBlockNumber((error: any, number:any) => {
                 if (error) throw error;
                 let latestBlockOnChain = number;
-                let blocks: any = [];
-                let currentDayBalance: any = {};
+                let progressInfo = new ProgressInfo(latestBlockOnChain, checkpoint);
 
-                process();
+                Config.web3.eth.getBlock(latestBlockOnChain, false, function (error: any, result: any) {
+                    if (error) throw error;
+                    let lastBlockTimestamp = moment.unix(result.timestamp).utc().endOf('day').unix();
 
-                function process() {
-                    if (currentBlockNumber > latestBlockOnChain) {
-                        console.log("At last block, sleeping ")
-                        setDelay(5000).then(() => {
-                            latestBlockOnChain = Config.web3.eth.getBlockNumber();
-                            process();
-                        });
-                    } else {
-                        blocks.push(starter.next(currentBlockNumber, currentDayTimestamp));
-                        currentBlockNumber++;
-                        if (blocks.length > maxConcurrentBlocks) {
-                            let maxValidBlock = 0;
-                            let maxBlockNumber = 0;
-                            let done = false;
-                            Promise.all(blocks).then((results: any) => {
-                                results.forEach((result: any) => {
-                                    if (result.block > maxBlockNumber) {
-                                        maxBlockNumber = result.block;
-                                    }
-                                    if ('status' in result) {
-                                        done = true;
-                                    } else {
-                                        if (result.block > maxValidBlock) {
-                                            maxValidBlock = result.block;
-                                        }
-                                        Object.keys(result.changes).forEach((address: any) => {
-                                            if (!(address in currentDayBalance)) {
-                                                currentDayBalance[address] = Config.web3.utils.toBN(0);
-                                            }
+                    let blocks: any = [];
+                    let currentDayBalance: any = {};
 
-                                            if (results[address] != 0) {
-                                                currentDayBalance[address] = currentDayBalance[address].add(result.changes[address]);
-                                            }
-                                        });
-                                    }
-                                });
-                                if (maxValidBlock === 0) maxValidBlock = maxBlockNumber;
-                                if (maxValidBlock === 0) {
-                                    console.log("We didn't find a maxValidBlock, WTF");
-                                    throw "Max block not found";
-                                }
-                                currentBlockNumber = maxValidBlock + 1;
-                                blocks = [];
-                                if (done) {
-                                    starter.validate(currentDayTimestamp, currentDayBalance);
+                    process();
 
-                                    Config.web3.eth.getBlock(currentBlockNumber, true).then((firstBlock: any) => {
-                                        const m = moment.unix(firstBlock.timestamp).utc().endOf('day');
-                                        currentDayTimestamp = m.unix();
-                                        console.log("Started new day: " + currentDayTimestamp + " on block " + currentBlockNumber);
-                                        currentDayBalance = {};
-                                        connection.query("replace into validate_status (id, current_full_validate_block) values (1, ?)", [currentBlockNumber], function (error: any, results: any, fields: any) {
-                                            if (error) {
-                                                throw error;
-                                            }
-                                        });
-                                        process();
-                                    });
-                                } else {
+                    function process() {
+                        if (currentBlockNumber % checkpoint === 0) {
+                            console.log(progressInfo.info(currentBlockNumber));
+                            progressInfo.checkpoint();
+                        }
+                        if (currentBlockNumber > latestBlockOnChain) {
+                            console.log("At last block, sleeping...");
+                            setDelay(5000).then(() => {
+                                latestBlockOnChain = Config.web3.eth.getBlockNumber();
+                                progressInfo = new ProgressInfo(latestBlockOnChain, checkpoint);
+                                Config.web3.eth.getBlock(latestBlockOnChain, false, function (error: any, result: any) {
+                                    if (error) throw error;
+                                    lastBlockTimestamp = moment.unix(result.timestamp).utc().endOf('day').unix();
                                     process();
-                                }
+                                });
                             });
                         } else {
-                            process();
+                            blocks.push(starter.next(currentBlockNumber, currentDayTimestamp));
+                            currentBlockNumber++;
+                            if (blocks.length > maxConcurrentBlocks) {
+                                let maxValidBlock = 0;
+                                let maxBlockNumber = 0;
+                                let done = false;
+                                Promise.all(blocks).then((results: any) => {
+                                    results.forEach((result: any) => {
+                                        if (result.block > maxBlockNumber) {
+                                            maxBlockNumber = result.block;
+                                        }
+                                        if ('status' in result) {
+                                            done = true;
+                                        } else {
+                                            if (result.block > maxValidBlock) {
+                                                maxValidBlock = result.block;
+                                            }
+                                            Object.keys(result.changes).forEach((address: any) => {
+                                                if (!(address in currentDayBalance)) {
+                                                    currentDayBalance[address] = Config.web3.utils.toBN(0);
+                                                }
+
+                                                if (results[address] != 0) {
+                                                    currentDayBalance[address] = currentDayBalance[address].add(result.changes[address]);
+                                                }
+                                            });
+                                        }
+                                    });
+                                    if (maxValidBlock === 0) maxValidBlock = maxBlockNumber;
+                                    if (maxValidBlock === 0) {
+                                        console.log("We didn't find a maxValidBlock, WTF");
+                                        throw "Max block not found";
+                                    }
+                                    currentBlockNumber = maxValidBlock + 1;
+                                    if ((currentBlockNumber % 500) === 0) {
+                                        console.log("Moving on to block: " + currentBlockNumber);
+                                    }
+                                    blocks = [];
+                                    if (done) {
+                                        starter.validate(currentDayTimestamp, currentDayBalance).then((res) => {});
+
+                                        Config.web3.eth.getBlock(currentBlockNumber, true).then((firstBlock: any) => {
+                                            const m = moment.unix(firstBlock.timestamp).utc().endOf('day');
+                                            currentDayTimestamp = m.unix();
+                                            console.log("Started new day: " + currentDayTimestamp + " on block " + currentBlockNumber);
+                                            currentDayBalance = {};
+                                            connection.query("replace into validate_status (id, current_full_validate_block) values (1, ?)", [currentBlockNumber], function (error: any, results: any, fields: any) {
+                                                if (error) {
+                                                    throw error;
+                                                }
+                                            });
+                                            process();
+                                        });
+                                    } else {
+                                        process();
+                                    }
+                                });
+                            } else {
+                                if (currentDayTimestamp === lastBlockTimestamp) {
+                                    starter.validate(currentDayTimestamp, currentDayBalance).then((res) => {});
+                                }
+                                process();
+                            }
                         }
                     }
-                }
+                });
             });
 
             console.log("Verification started");
