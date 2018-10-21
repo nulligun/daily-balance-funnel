22a1e9404cfb3ae0acaeb82038f6e8e483c993e4
Steve Mulligan <steve@nonlocal.ca>
2018-10-21 10:33:24 -0400

    including instructions for how to create mysql tables without having to use python3

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ This is a node app that will monitor a Eth compatible network for balance state 
 
 ## About
 
-This a fork of https://github.com/TrustWallet/trust-ray that only includes components needed to scan the blockchain for transactions. It's not
-a drop in replacement.  The HTTP api has been removed and it only stores a daily balance change in SQL as opposed to full transaction details.
+This started as a fork of https://github.com/TrustWallet/trust-ray that only included components needed to scan the blockchain for transactions. Eventually
+this was replaced with a forward only scanner.
 
 It was created to be the backend for the [Ellaism Historical Balance Tool](https://github.com/stevemulligan/bellance)
 
@@ -31,6 +31,25 @@ npm run build
 Edit config/default.json to point to your mongo instance, MySQL server and Eth RPC Endpoint.
 
 Use `python3 create_tables.py` from the [Ellaism Historial Balance Tool](https://github.com/stevemulligan/bellance) to create the database tables.
+
+Or you can use the following to create the tables from the mysql command line:
+
+```
+
+mysql> create database funnel;
+Query OK, 1 row affected (0.00 sec)
+
+mysql> grant all on funnel.* to funnel identified by 'YOUR__PASSWORD__HERE';
+Query OK, 0 rows affected (0.01 sec)
+
+mysql> flush privileges;
+Query OK, 0 rows affected (0.00 sec)
+
+```
+
+Then import funnel.sql into the database you just created.
+
+`mysql -u funnel -p funnel < funnel.sql`
 
 
 ## Running
---
+++ funnel.sql
@@ -0,0 +1,100 @@
+-- MySQL dump 10.13  Distrib 5.6.23, for osx10.8 (x86_64)
+--
+-- Host: localhost    Database: funnel
+-- ------------------------------------------------------
+-- Server version	5.6.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `addresses`
+--
+
+DROP TABLE IF EXISTS `addresses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `addresses` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `address` char(42) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ix_addresses_address` (`address`),
+  UNIQUE KEY `address_idx` (`address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `addresses`
+--
+
+LOCK TABLES `addresses` WRITE;
+/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `balances`
+--
+
+DROP TABLE IF EXISTS `balances`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `balances` (
+  `address_id` int(10) unsigned NOT NULL,
+  `balance_date` bigint(20) unsigned NOT NULL,
+  `delta` decimal(32,0) NOT NULL,
+  PRIMARY KEY (`address_id`,`balance_date`),
+  CONSTRAINT `balances_ibfk_1` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `balances`
+--
+
+LOCK TABLES `balances` WRITE;
+/*!40000 ALTER TABLE `balances` DISABLE KEYS */;
+/*!40000 ALTER TABLE `balances` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `validate_status`
+--
+
+DROP TABLE IF EXISTS `validate_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `validate_status` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `current_full_validate_block` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `validate_status`
+--
+
+LOCK TABLES `validate_status` WRITE;
+/*!40000 ALTER TABLE `validate_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `validate_status` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-21 10:29:03
